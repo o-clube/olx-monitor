fab8d68e130e2a975296d67bd2aff1b1d6e88841
Augusto Carmo <augusto@massimo.ag>
2020-12-28 11:50:10 -0300

    config sample

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ let config = {};
 
 config.urls = [
 	'https://pr.olx.com.br/regiao-de-curitiba-e-paranagua/computadores-e-acessorios?pe=500&ps=100&q=unifi',
-	'https://sp.olx.com.br/sao-paulo-e-regiao/computadores-e-acessorios?pe=500&ps=100&q=unifi'
+    'https://sp.olx.com.br/sao-paulo-e-regiao/computadores-e-acessorios?pe=500&ps=100&q=unifi',
+    'https://sp.olx.com.br/sao-paulo-e-regiao?q=chromecast'
 ]
 
 config.telegramChatID = process.env.TELEGRAM_CHAT_ID
